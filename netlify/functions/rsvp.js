246d791302b07db817318ad56f512029de4c57ed
Tobie-Rathbun <tsrathbun@gmail.com>
2025-01-16 14:56:04 -0700

    moved database to netlify folder

--- netlify/functions/rsvp.js
+++ netlify/functions/rsvp.js
@@ -1,5 +1,7 @@
 const sqlite3 = require("sqlite3").verbose();
-const db = new sqlite3.Database("./rsvp.db");
+const path = require("path");
+const dbPath = path.resolve(__dirname, "data", "rsvp.db");
+const db = new sqlite3.Database(dbPath);
 
 exports.handler = async (event) => {
   if (event.httpMethod === "OPTIONS") {
