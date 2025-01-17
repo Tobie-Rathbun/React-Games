b1f8bf09febf487c1a19460be33c149fe97b4906
Tobie-Rathbun <tsrathbun@gmail.com>
2025-01-17 04:27:40 -0700

    heavily locally tested solution with cockroachdb

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import type { Configuration as WebpackConfig } from 'webpack';
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config: WebpackConfig) => {
+    if (config.resolve) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@babylonjs/core': path.resolve('node_modules/@babylonjs/core'),
+        '@babylonjs/loaders': path.resolve('node_modules/@babylonjs/loaders'),
+      };
+    }
+    return config;
+  },
+  outputFileTracingIncludes: {
+    '/_next/static/chunks/**': [
+      'node_modules/@babylonjs/core/**/*',
+      'node_modules/@babylonjs/loaders/**/*',
+    ],
+  },
 };
 
 export default nextConfig;
